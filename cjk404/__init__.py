e4aa91089c6c636453aa2de5f35d05f1a5ba7422
Bartłomiej Szkałuba <bartek.szkaluba@gmail.com>
2024-03-08 10:55:09 +0100

    version bump

--- cjk404/__init__.py
+++ cjk404/__init__.py
@@ -4,6 +4,6 @@
 
 __author__ = """Grzegorz Król, Filip Woźniak"""
 __email__ = "gk@kachange.eu; fw@kachange.eu"
-release = ["23", "7", "2"]
+release = ["24", "3", "1"]
 __version__ = "{0}.{1}.{2}".format(release[0], release[1], release[2])
 __shortversion__ = "{0}.{1}".format(release[0], release[1])
